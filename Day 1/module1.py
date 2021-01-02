a70b8cd2456782e8a7ef323634ced8ee067b4c02
sanskar <lathersanskar@gmail.com>
2021-01-02 16:53:28 +0530

    Day 1

---
+++ Day 1/module1.py
@@ -0,0 +1,7 @@
+
+#Day 1 Practisisng Python I/O
+print("Random Band Name Genrator\n")
+#input() function to take input
+city=input("Enter name of your city\n")
+pet=input("Enter the name of your pet\n")
+print("Your band name is "+city+" "+pet)
